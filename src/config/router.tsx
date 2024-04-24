c951f917ac3f446eab08610db6b7aea971b79668
Ivan Andrianto <ivandriancode@gmail.com>
2024-04-24 21:54:05 +0700

    add: react router

---
+++ src/config/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import ErrorPage from '../pages/ErrorPage'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        // children: [
+        //     {
+        //         path: 'about',
+        //         element: <AboutPage />,
+        //     },
+        // ],
+    },
+])
